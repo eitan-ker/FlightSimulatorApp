ce70b23d02f9bc45d802754f13500ae33cc0abe0
eitan-ker <58944748+eitan-ker@users.noreply.github.com>
2020-03-23 12:40:34 +0200

    added callses to vm, and model

--- FlightSimulatorApp/Model/ItelnetClient.cs
+++ FlightSimulatorApp/Model/ItelnetClient.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FlightSimulatorApp.ViewModel
+namespace FlightSimulatorApp.Model
 {
-    class FlightSimulatorAppViewModel
+    interface ItelnetClient
     {
     }
 }
