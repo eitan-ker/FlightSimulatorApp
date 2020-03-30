7a07e9a91c4a53dffe1edfb056de29cf140b67c0
menimen <menash9288@gmail.com>
2020-03-30 14:57:53 +0300

    changed dashboard visibility of variables - from now on, if you are disconnected then the value will be "ERR"

--- FlightSimulatorApp/Model/ISimApp.cs
+++ FlightSimulatorApp/Model/ISimApp.cs
@@ -11,15 +11,15 @@ namespace FlightSimulatorApp.Model
     public interface ISimApp : INotifyPropertyChanged
     {
         string Indicated_heading_deg { get; set; }
-        double Gps_indicated_vertical_speed { get; set; }
-        double Gps_indicated_ground_speed_kt { get; set; }
-        double Airspeed_indicator_indicated_speed_kt { get; set; }
-        double Gps_indicated_altitude_ft { get; set; }
-        double Attitude_indicator_internal_roll_deg { get; set; }
-        double Attitude_indicator_internal_pitch_deg { get; set; }
-        double Altimeter_indicated_altitude_ft { get; set; }
-        double Latitude_deg { get; set; }
-        double Longitude_deg { get; set; }
+        string Gps_indicated_vertical_speed { get; set; }
+        string Gps_indicated_ground_speed_kt { get; set; }
+        string Airspeed_indicator_indicated_speed_kt { get; set; }
+        string Gps_indicated_altitude_ft { get; set; }
+        string Attitude_indicator_internal_roll_deg { get; set; }
+        string Attitude_indicator_internal_pitch_deg { get; set; }
+        string Altimeter_indicated_altitude_ft { get; set; }
+        string Latitude_deg { get; set; }
+        string Longitude_deg { get; set; }
         string Locations { get; set; }
         void FlyPlane(double elevator, double rudder);
         void moveThrottle(double throttle);
